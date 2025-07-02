8778c01095fc62b56022eb90cc810d2d640ca70e
Sam Touba <104673765+ToubaSlam@users.noreply.github.com>
2025-07-02 15:55:00 -0500

    Add negative create tests for all domains

---
+++ src/test/java/testcases/houseHolding/TC09_CreateNewHousehold_NegativeData.java
@@ -0,0 +1,97 @@
+package testcases.houseHolding;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.Test;
+import testcases.TestBase;
+import pojo.CreateHousehold;
+
+import java.util.Map;
+
+import static builder.RequestBuilder.createRequestSpecification;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.lessThan;
+import static util.Enpoint.HOUSEHOLDS;
+
+public class TC09_CreateNewHousehold_NegativeData extends TestBase {
+
+    /**
+     * Create household with empty name.
+     * Expect 400 Bad Request.
+     */
+    @Test(priority = 1, description = "Create household with empty name")
+    public void createHousehold_EmptyName_N() throws JsonProcessingException {
+        CreateHousehold household = new CreateHousehold().setName("");
+
+        Map<String, String> headers = Map.of(
+                "Content-Type", "application/json",
+                "g-token", "ROM831ESV"
+        );
+        Map<String, String> queryParameters = Map.of();
+        ObjectMapper mapper = new ObjectMapper();
+
+        given()
+                .spec(createRequestSpecification(headers, queryParameters))
+                .body(mapper.writeValueAsString(household))
+                .when()
+                .post(HOUSEHOLDS)
+                .then()
+                .statusCode(400)
+                .body(containsString("name"))
+                .time(lessThan(5000L));
+    }
+
+    /**
+     * Create household with name too long.
+     * Expect 400 Bad Request.
+     */
+    @Test(priority = 2, description = "Create household with long name")
+    public void createHousehold_NameTooLong_N() throws JsonProcessingException {
+        String longName = "A".repeat(300);
+        CreateHousehold household = new CreateHousehold().setName(longName);
+
+        Map<String, String> headers = Map.of(
+                "Content-Type", "application/json",
+                "g-token", "ROM831ESV"
+        );
+        Map<String, String> queryParameters = Map.of();
+        ObjectMapper mapper = new ObjectMapper();
+
+        given()
+                .spec(createRequestSpecification(headers, queryParameters))
+                .body(mapper.writeValueAsString(household))
+                .when()
+                .post(HOUSEHOLDS)
+                .then()
+                .statusCode(400)
+                .body(containsString("name"))
+                .time(lessThan(5000L));
+    }
+
+    /**
+     * Create household with null name.
+     * Expect 400 Bad Request.
+     */
+    @Test(priority = 3, description = "Create household with null name")
+    public void createHousehold_NullName_N() throws JsonProcessingException {
+        CreateHousehold household = new CreateHousehold().setName(null);
+
+        Map<String, String> headers = Map.of(
+                "Content-Type", "application/json",
+                "g-token", "ROM831ESV"
+        );
+        Map<String, String> queryParameters = Map.of();
+        ObjectMapper mapper = new ObjectMapper();
+
+        given()
+                .spec(createRequestSpecification(headers, queryParameters))
+                .body(mapper.writeValueAsString(household))
+                .when()
+                .post(HOUSEHOLDS)
+                .then()
+                .statusCode(400)
+                .body(containsString("name"))
+                .time(lessThan(5000L));
+    }
+}
---
+++ src/test/java/testcases/users/TC09_CreateNewUser_NegativeData.java
@@ -0,0 +1,105 @@
+package testcases.users;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.Test;
+import testcases.TestBase;
+import pojo.CreateUser;
+
+import java.util.Map;
+
+import static builder.RequestBuilder.createRequestSpecification;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.lessThan;
+import static util.Enpoint.USERS;
+
+public class TC09_CreateNewUser_NegativeData extends TestBase {
+
+    /**
+     * Create user with invalid email format.
+     * Expect 400 Bad Request.
+     */
+    @Test(priority = 1, description = "Create user with invalid email")
+    public void createUser_InvalidEmail_N() throws JsonProcessingException {
+        CreateUser user = new CreateUser()
+                .setFirstName("John")
+                .setLastName("Doe")
+                .setEmail("invalid_email");
+
+        Map<String, String> headers = Map.of(
+                "Content-Type", "application/json",
+                "g-token", "ROM831ESV"
+        );
+        Map<String, String> queryParameters = Map.of();
+        ObjectMapper mapper = new ObjectMapper();
+
+        given()
+                .spec(createRequestSpecification(headers, queryParameters))
+                .body(mapper.writeValueAsString(user))
+                .when()
+                .post(USERS)
+                .then()
+                .statusCode(400)
+                .body(containsString("email"))
+                .time(lessThan(5000L));
+    }
+
+    /**
+     * Create user with null first name.
+     * Expect 400 Bad Request.
+     */
+    @Test(priority = 2, description = "Create user with null first name")
+    public void createUser_NullFirstName_N() throws JsonProcessingException {
+        CreateUser user = new CreateUser()
+                .setFirstName(null)
+                .setLastName("Doe")
+                .setEmail("john@example.com");
+
+        Map<String, String> headers = Map.of(
+                "Content-Type", "application/json",
+                "g-token", "ROM831ESV"
+        );
+        Map<String, String> queryParameters = Map.of();
+        ObjectMapper mapper = new ObjectMapper();
+
+        given()
+                .spec(createRequestSpecification(headers, queryParameters))
+                .body(mapper.writeValueAsString(user))
+                .when()
+                .post(USERS)
+                .then()
+                .statusCode(400)
+                .body(containsString("firstName"))
+                .time(lessThan(5000L));
+    }
+
+    /**
+     * Create user with missing last name.
+     * Expect 400 Bad Request.
+     */
+    @Test(priority = 3, description = "Create user with empty last name")
+    public void createUser_EmptyLastName_N() throws JsonProcessingException {
+        CreateUser user = new CreateUser()
+                .setFirstName("John")
+                .setLastName("")
+                .setEmail("john@example.com");
+
+        Map<String, String> headers = Map.of(
+                "Content-Type", "application/json",
+                "g-token", "ROM831ESV"
+        );
+        Map<String, String> queryParameters = Map.of();
+        ObjectMapper mapper = new ObjectMapper();
+
+        given()
+                .spec(createRequestSpecification(headers, queryParameters))
+                .body(mapper.writeValueAsString(user))
+                .when()
+                .post(USERS)
+                .then()
+                .statusCode(400)
+                .body(containsString("lastName"))
+                .time(lessThan(5000L));
+    }
+}
